02b000d2fe9ac38587cb45ae779c3859b1031c19
phramusca <phramusca@gmail.com>
2026-03-19 11:09:12 +0000

    enhance build (version)

--- build-flatpak-https-handler.sh
+++ build-flatpak-https-handler.sh
@@ -6,5 +6,8 @@ chmod 755 usr/bin/flatpak-https-handler
 chmod 644 usr/share/applications/flatpak-https-handler.desktop
 chmod 755 DEBIAN/postinst
 command -v fakeroot >/dev/null 2>&1 || { echo "Error: fakeroot is required (sudo apt install fakeroot)." >&2; exit 1; }
-fakeroot dpkg-deb --build . ..
-echo "Package built: ../flatpak-https-handler_1.0_all.deb"
+package_name="$(awk -F': ' '/^Package:/ { print $2 }' DEBIAN/control)"
+package_version="$(awk -F': ' '/^Version:/ { print $2 }' DEBIAN/control)"
+output_file="./${package_name}_${package_version}_all.deb"
+fakeroot dpkg-deb --build . "$output_file"
+echo "Package built: $output_file"
